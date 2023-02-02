b19a1ea9907c08fe55647d98c3d06e72e3882a67
Jon McGill <jonaaronmcgill@gmail.com>
2023-02-02 06:44:05 -0600

    Add project files. n

--- Source.cpp
+++ Source.cpp
@@ -5,15 +5,99 @@
 int nScreenWidth = 120;
 int nScreenHeight = 40;
 
+float fPlayerX = 0.0f; // Player X Cordanance
+float fPlayerY = 0.0f; // Player Y Cordanance
+float fPlayerA = 0.0f; // Player Angle
+
+// Map Dimentions
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+
+// Field of view = PIE / 4;
+float fFOV = 3.14159 / 4;
+float fDepth = 16.0f;
+
 int main()
 {
 	// Create Screen Buffer
-	wchar_t* Screen = new wchar_t[nScreenWidth * nScreenHeight];
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
 	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(hConsole);
-	DWORD dwByteWritten = 0;
+	DWORD dwBytesWritten = 0;
 
 
+	// Init wstring 1D array
+	std::wstring map = L"Hello";
+	
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
+
+
+
+
+	// Game Loop
+	while (1)
+	{
+		for (int x = 0; x < nScreenWidth; x++)
+		{
+			// For each column, calculate the projected ray angle into world space
+			float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+			float fDistanceToWall = 0.f;
+			bool bHitWall = false;
+
+			float fEyeX = sinf(fRayAngle); // Unit vector for ray in player space
+			float fEyeY = cosf(fRayAngle);
+
+			// Raytrace check increments until it hits a cell(wall)
+			while (!bHitWall && fDistanceToWall < fDepth)
+			{
+				fDistanceToWall += 0.1f;
+
+				int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+				int nTestY = (int)(fPlayerY + fEyeY * fDistanceToWall);
+
+				// Test if ray is out of bounds
+				if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
+				{
+					bHitWall = true;
+					fDistanceToWall = fDepth;
+				}
+				else
+				{
+					// Ray is inbounds so test to see if the ray cell is a wall block
+					if (map[nTestY * nMapWidth + nTestX] == '#')
+					{
+						bHitWall = true;
+					}
+				}
+			}
+		}
+
+		// Sets screens last element to 0. (To escape)
+		screen[nScreenWidth * nScreenHeight - 1] = '\0';
+
+		// Writes to our buffer. (HANDLE, buffer, how many bytes?, {0,0}cordanance(where to start writing), BytesWritten)
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+
+	}
+
+	
 	return 0;
 }
  
